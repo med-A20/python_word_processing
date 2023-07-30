c2f3ad797e38af5fce00b89e34621b6a97dcbe37
Amchia_Mohamed <amchiamohamed41@gmail.com>
2023-07-30 16:04:16 +0100

    Initial commit_9

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask 
-from .routes import main
+from .routes import app
 
 def create_app():
     app = Flask(__name__)
 
-    app.register_blueprint(main)
+    app.register_blueprint(app)
 
     return app
